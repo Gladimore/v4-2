00bb44c845f3e2012b9f530e61993c5e8f054d2b
Gladimore <105724638+Gladimore@users.noreply.github.com>
2023-12-13 09:34:19 -0600

    Update blank.js

--- public/js/blank.js
+++ public/js/blank.js
@@ -1,27 +1,17 @@
-const name = 'main.html'
-const url = location.href + name
-
-function inFrame(){
-  return location.href !== window.parent.location.href
-}
+const fileName = 'main.html'
+const path = "/" + fileName 
 
 function blank(){
-      var w = window.open("about:blank", "_blank")
-      w.document.write('<script>document.title = "about:blank";</script>' +
-  '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="'+url+'"></iframe>');
+  var w = window.open("about:blank", "_blank")
+    w.document.write('<script>document.title = "about:blank";</script>' +
+  '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="'+path+'"></iframe>');
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  try {
-    if (inFrame()){
-      blank();
-      location.href = 'https://drive.google.com/drive/'
-    } else{
-      if (!location.href.includes(name)){
-        location.href = '/' + name
-      }
-    }
-} catch (err){
-  alert(err)
+if (window.self === window.parent){  
+  blank();
+  location.href = 'https://drive.google.com/drive'
+} else {
+  if (location.pathname !== path){
+    location.href = path
+  }
 }
-})
