a5775fabf60d16c54e18e03dd73cb09b0c963102
Gladimore <105724638+Gladimore@users.noreply.github.com>
2023-12-14 09:28:44 -0600

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -75,7 +75,7 @@ var apps = [
       'id': 'chess',
       'title': 'Chess.com',
       'url': 'https://chess.com',
-      'image': 'https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/SamCopeland/phpmeXx6V.png'
+      'image': 'https://ibb.co/7v1wYbb'
     },
     {
       'id': 'movie-web',
