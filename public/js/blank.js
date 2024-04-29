ae12a7bfb9092aa67a033bc8c92764695ec79181
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-04-29 09:23:33 -0500

    Update blank.js

--- public/js/blank.js
+++ public/js/blank.js
@@ -1,5 +1,4 @@
 const path = "/main.html"
-const valid_ids_json = "/assets/data/valid-ids.json"
 
 export function blank(url, sentWindow = window) {
   var w = window.open("about:blank", "_blank")
@@ -19,36 +18,5 @@ function inFrame() {
 function ensure() {
   if (inFrame() && location.pathname !== path) {
     location.href = path
-  }
-}
-
-export function login() {
-  try {
-    fetch(valid_ids_json)
-      .then((res) => res.json())
-      .then((data = {}) => {
-        const nav_info = window.navigator
-        const screen_info = window.screen
-        var uid = nav_info.mimeTypes.length
-        uid += nav_info.userAgent.replace(/\D+/g, '')
-        uid += nav_info.plugins.length
-        uid += screen_info.height || ""
-        uid += screen_info.width || ""
-        uid += screen_info.pixelDepth || ""
-
-        console.log(uid)
-
-        if (data.includes(uid)) {
-          //valid
-          ensure()
-        } else {
-          console.log('visitorId not found')
-
-          let par = (!inFrame() && window) || inFrame() && window.parent
-          par.location.href = "https://gladimore.github.io/getId/"
-        }
-      })
-  } catch (err) {
-    console.warn('cant fetch?')
   }
 }
