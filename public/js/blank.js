2925d9dd447439ab5792e82af7d168b6562a070a
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-04-29 09:34:03 -0500

    Update blank.js

--- public/js/blank.js
+++ public/js/blank.js
@@ -1,13 +1,17 @@
 const path = "/main.html"
 
 export function blank(url, sentWindow = window) {
-  var w = window.open("about:blank", "_blank")
+  ensure()
+  
+  if (!inFrame()){
+    var w = window.open("about:blank", "_blank")
   w.document.write('<script>document.title = "about:blank";</script>' +
     '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
   try{
     sentWindow.close()
   } catch(e){
     sentWindow.locaion.href = "//drive.google.com/drive"
+  }
   }
 }
 
