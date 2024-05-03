153c32303df7ee6a000a2f5870a5a2c5550cdcb5
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-05-03 10:11:06 -0500

    Update blank.js

--- public/js/blank.js
+++ public/js/blank.js
@@ -1,18 +1,16 @@
-const path = "/main.html"
+const path = "https://aluu.xyz/en/"
 
-export function blank(url) {
-  url = url || path
+function blank(){
   var w = window.open("about:blank", "_blank")
-  w.document.write('<script>document.title = "about:blank";</script>' +
-    '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
+    w.document.write('<script>document.title = "about:blank";</script>' +
+  '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%; referrerpolicy="no-referrer";" src="'+path+'"></iframe>');
 }
 
-function inFrame() {
-  return window.self !== window.parent
-}
-
-function ensure() {
-  if (inFrame() && location.pathname !== path) {
+if (window.self === window.parent){  
+  blank();
+  location.href = "//google.com"
+} else {
+  if (location.pathname !== path){
     location.href = path
   }
 }
