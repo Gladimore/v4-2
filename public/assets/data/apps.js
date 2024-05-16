05fdc45aa3471b067cdc68d85530f9a44169cdd2
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-05-16 09:18:15 -0500

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -7,68 +7,51 @@ var apps = [
     {
       "id": "google",
       "title": "Google",
-      "url": "https://www.google.com/?safe=active&ssui=on",
-      'image': 'https://img.freepik.com/free-icon/search_318-265146.jpg'
-      
+      "url": "https://www.google.com/?safe=active&ssui=on"
     },
     {
       "id": "discord",
       "title": "Discord",
-      "url": "https://discord.com/app",
-      'image': 'https://static.vecteezy.com/system/resources/previews/006/892/625/original/discord-logo-icon-editorial-free-vector.jpg'
-    },
+      "url": "https://discord.com/app"
     {
       "id": "telegram",
       "title": "Telegram",
-      "url": "https://web.telegram.org/a/",
-      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png'
+      "url": "https://web.telegram.org/a/"
     }, 
     {
       "id": "guilded",
       "title": "Guilded",
-      "url": "https://www.guilded.gg/",
-      'image': 'https://yt3.googleusercontent.com/cPlwwgZkQQgHRLAg7jvJG4yKGLDId6NH7H_S87TCoEV_dbsF9Me_4YSBL5DyMsndx6hzoZxHUw=s176-c-k-c0x00ffffff-no-rj'
+      "url": "https://www.guilded.gg/"
     },
     {
       "id": "geforce",
       "title": "GeForce NOW (may not work)",
       "url": "https://play.geforcenow.com",
-      'image': 'https://raw.githubusercontent.com/proudparrot2/cdn/main/8z9zeDIT_400x400-removebg-preview.png'
     },
     {
       'id': 'vscode',
       'title': 'Visual Studio Code',
-      'url': 'https://vscode.dev/',
-      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png'
+      'url': 'https://vscode.dev/'
     },
     {
       "id": "tiktok",
       "title": "TikTok",
-      "url": "https://tiktok.com",
-      'image': 'https://sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png'
+      "url": "https://tiktok.com"
     },
     {
       "id": "youtube",
       "title": "YouTube",
-      "url": "https://youtube.com",
-      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/YouTube_social_red_square_%282017%29.svg/2048px-YouTube_social_red_square_%282017%29.svg.png'
+      "url": "https://youtube.com"
     },
     {
       'id': 'spotify',
       'title': 'Spotify',
-      'url': 'https://open.spotify.com/browse',
-      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png'
+      'url': 'https://open.spotify.com/browse'
     },
     {
       'id': 'movie-web',
       'title': 'CineGo',
-      'url': 'https://cinego.tv/',
-      'image': 'https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://cinego.tv&size=256'
-    },
-    {
-        'id': 'test',
-        'title': 'Test',
-        'url': 'https://google.com'
+      'url': 'https://cinego.tv/'
     }
   ]
   
