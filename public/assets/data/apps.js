16c42ea5f253c7b0fd259bc02417b304e0ebe525
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-05-16 09:45:17 -0500

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -1,67 +1,72 @@
-var apps = const data = [
-    {
-      id: 'customapp',
-      title: 'Add custom app',
-      image: 'assets/images/add.png'
-    },
-    {
-      id: 'google',
-      title: 'Google',
-      url: 'https://www.google.com/?safe=active&ssui=on'
-    },
-    {
-      id: 'discord',
-      title: 'Discord',
-      url: 'https://discord.com/app'
-    },
-    {
-      id: 'telegram',
-      title: 'Telegram',
-      url: 'https://web.telegram.org/a/'
-    }, 
-    {
-      id: 'guilded',
-      title: 'Guilded',
-      url: 'https://www.guilded.gg/'
-    },
-    {
-      id: 'geforce',
-      title: 'GeForce NOW (may not work)',
-      url: 'https://play.geforcenow.com'
-    },
-    {
-      id: 'vscode',
-      title: 'Visual Studio Code',
-      url: 'https://vscode.dev/'
-    },
-    {
-      id: 'tiktok',
-      title: 'TikTok',
-      url: 'https://tiktok.com'
-    },
-    {
-      id: 'youtube',
-      title: 'YouTube',
-      url: 'https://youtube.com'
-    },
-    {
-      id: 'spotify',
-      title: 'Spotify',
-      url: 'https://open.spotify.com/browse'
-    },
-    {
-      id: 'movie-web',
-      title: 'CineGo',
-      url: 'https://cinego.tv/'
-    },
+var apps = [{
+    id: 'customapp',
+    title: 'Add custom app',
+    image: 'assets/images/add.png'
+  },
+  {
+    title: 'Google',
+    url: 'https://www.google.com/?safe=active&ssui=on'
+  },
+  {
+    title: 'Discord',
+    url: 'https://discord.com/app'
+  },
+  {
+    title: 'Telegram',
+    url: 'https://web.telegram.org/a/'
+  },
+  {
+    title: 'Guilded',
+    url: 'https://www.guilded.gg/'
+  },
+  {
+    title: 'GeForce NOW (may not work)',
+    url: 'https://play.geforcenow.com'
+  },
+  {
+    title: 'Visual Studio Code',
+    url: 'https://vscode.dev/'
+  },
+  {
+    title: 'TikTok',
+    url: 'https://tiktok.com'
+  },
+  {
+    id: 'youtube',
+    title: 'YouTube',
+    url: 'https://youtube.com'
+  },
+  {
+    title: 'Spotify',
+    url: 'https://open.spotify.com/browse'
+  },
+  {
+    title: 'CineGo',
+    url: 'https://cinego.tv/'
+  },
 ]
-  
-  var customapps = JSON.parse(localStorage.getItem("customapps"))
-  if (customapps) customapps.forEach(app => {
-    apps.push(app)
-  })
 
-    
-  
-  console.log("Loaded " + apps.length + " apps")
-  if (customapps) console.log("Loaded " + customapps.length + " custom apps")
+var customapps = JSON.parse(localStorage.getItem("customapps"))
+if (customapps) customapps.forEach(app => {
+  apps.push(app)
+})
+
+async function assignIds() {
+  for (let i = 0; i < apps.length; i++) {
+    let app = apps[i];
+
+    if (app.id === undefined) {
+      await fetch('https://www.uuidtools.com/api/generate/v1')
+        .then((res) => res.json())
+        .then((data) => {
+          app.id = data[0];
+        });
+    }
+    console.log(app);
+  }
+}
+
+assignIds().then(() => {
+  console.log("Loaded " + apps.length + " apps");
+  if (customapps) console.log("Loaded " + customapps.length + " custom apps");
+});
