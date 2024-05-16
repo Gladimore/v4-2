839dc5f1c147f1c4103163ecb8e76c7a955a31ae
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-05-16 14:41:00 -0500

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -60,6 +60,11 @@ var apps = [{
   }
 ]
 
+for (let i = 0; i<apps.length; i++){
+	let app = apps[i]
+  app.image = `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${app.url}&size=256`
+}
+
 var customapps = JSON.parse(localStorage.getItem("customapps"))
 if (customapps) customapps.forEach(app => {
   apps.push(app)
