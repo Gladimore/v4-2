4c8991a2bfbc5626b1a4aa64d09a55e919e3fa90
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-05-16 09:27:18 -0500

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -1,64 +1,67 @@
-var apps = [
+var apps = const data = [
     {
-      'id': 'customapp',
-      'title': 'Add custom app',
-      'image': 'assets/images/add.png'
+      id: 'customapp',
+      title: 'Add custom app',
+      image: 'assets/images/add.png'
     },
     {
-      "id": "google",
-      "title": "Google",
-      "url": "https://www.google.com/?safe=active&ssui=on"
+      id: 'google',
+      title: 'Google',
+      url: 'https://www.google.com/?safe=active&ssui=on'
     },
     {
-      "id": "discord",
-      "title": "Discord",
-      "url": "https://discord.com/app"
+      id: 'discord',
+      title: 'Discord',
+      url: 'https://discord.com/app'
+    },
     {
-      "id": "telegram",
-      "title": "Telegram",
-      "url": "https://web.telegram.org/a/"
+      id: 'telegram',
+      title: 'Telegram',
+      url: 'https://web.telegram.org/a/'
     }, 
     {
-      "id": "guilded",
-      "title": "Guilded",
-      "url": "https://www.guilded.gg/"
+      id: 'guilded',
+      title: 'Guilded',
+      url: 'https://www.guilded.gg/'
     },
     {
-      "id": "geforce",
-      "title": "GeForce NOW (may not work)",
-      "url": "https://play.geforcenow.com",
+      id: 'geforce',
+      title: 'GeForce NOW (may not work)',
+      url: 'https://play.geforcenow.com'
     },
     {
-      'id': 'vscode',
-      'title': 'Visual Studio Code',
-      'url': 'https://vscode.dev/'
+      id: 'vscode',
+      title: 'Visual Studio Code',
+      url: 'https://vscode.dev/'
     },
     {
-      "id": "tiktok",
-      "title": "TikTok",
-      "url": "https://tiktok.com"
+      id: 'tiktok',
+      title: 'TikTok',
+      url: 'https://tiktok.com'
     },
     {
-      "id": "youtube",
-      "title": "YouTube",
-      "url": "https://youtube.com"
+      id: 'youtube',
+      title: 'YouTube',
+      url: 'https://youtube.com'
     },
     {
-      'id': 'spotify',
-      'title': 'Spotify',
-      'url': 'https://open.spotify.com/browse'
+      id: 'spotify',
+      title: 'Spotify',
+      url: 'https://open.spotify.com/browse'
     },
     {
-      'id': 'movie-web',
-      'title': 'CineGo',
-      'url': 'https://cinego.tv/'
-    }
-  ]
+      id: 'movie-web',
+      title: 'CineGo',
+      url: 'https://cinego.tv/'
+    },
+]
   
   var customapps = JSON.parse(localStorage.getItem("customapps"))
   if (customapps) customapps.forEach(app => {
     apps.push(app)
   })
+
+    
   
   console.log("Loaded " + apps.length + " apps")
   if (customapps) console.log("Loaded " + customapps.length + " custom apps")
