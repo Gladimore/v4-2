d829d03d77005eb0933b308670eb23b1c949ef87
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-10-03 09:21:15 -0500

    Update blank.js

--- public/js/blank.js
+++ public/js/blank.js
@@ -1,16 +1,28 @@
 const path = "/main.html"
 
-function blank(){
+function blank() {
   var w = window.open("about:blank", "_blank")
-    w.document.write('<script>document.title = "about:blank";</script>' +
-  '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%; referrerpolicy="no-referrer";" src="'+path+'"></iframe>');
+  w.document.write(
+    '<script>document.title = "about:blank";</script>' +
+      '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%; referrerpolicy="no-referrer";" src="' +
+      path +
+      '"></iframe>',
+  )
 }
 
-if (window.self === window.parent){  
-  blank();
-  location.href = "//google.com"
-} else {
-  if (location.pathname !== path){
-    location.href = path
+const name = "aboutCloak"
+const value = "false"
+
+const get = localStorage.getItem(name)
+const bool = get == "true" ? true : false
+
+if (bool || get == null) {
+  if (window.self === window.parent) {
+    blank()
+    location.href = "//google.com"
+  } else {
+    if (location.pathname !== path) {
+      location.href = path
+    }
   }
 }
